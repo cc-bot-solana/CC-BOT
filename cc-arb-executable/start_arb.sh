cb0326c64b7f212b49fe3307b2cbbaeebf2bd2e6
cc-bot-solana <cc.bot.solana@gmail.com>
2025-04-24 05:43:23 +0300

    Update start_arb.sh

--- cc-arb-executable/start_arb.sh
+++ cc-arb-executable/start_arb.sh
@@ -6,6 +6,8 @@ export SEND_TXN_RPC_URL= #YOUR_RPC_URL
 export QUOTE_REQUEST_INTERVAL_MS=15
 export DEFAULT_SOL_PRICE=200
 export CHECK_ATA_RPC_URL= #YOUR_RPC_URL
+#export PROXY_TYPE=socks5 #if your proxy type support socls5 you should enable
+
 
 # Execute the constructed command
 CMD="./cc-arbitrage-bot"
